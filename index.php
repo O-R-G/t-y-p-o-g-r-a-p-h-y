91512d9af4cdc3dd41cdce4e032615914eb07ae3
reinfurt <reinfurt@o-r-g.com>
2020-08-13 12:06:44 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?
+// $uri = explode('/', $_SERVER['REQUEST_URI']);
+
+// show the things
+require_once("views/head.php");
+require_once("views/main.php");
+require_once("views/badge.php");
+require_once("views/foot.php");
+?>
