4b7a79e0ebc84fb7d595fd7aa844e09f6415d7e9
wei <wei@o-r-g.com>
2025-10-28 12:56:43 +0800

    update gitignore and remove cached media files. update screenfull

--- static/php/config.php
+++ static/php/config.php
@@ -3,7 +3,14 @@
     site-specific config
 */
 
+$site_title = 'T-y-p-o-g-r-a-p-h-y';
 $head = 'VIS 215';
 $site = 'T-y-p-o-g-r-a-p-h-y';
 $home = $head . ", " . $site;
+$card_default = '/media/jpg/card-default.jpg';
+$logo_src = '/media/jpg/logo.jpg';
+$description = 'Hello, w-w-w-orld.';
+$site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+$site_url .= '://' . $_SERVER['SERVER_NAME'];
+$og_locale = 'en_US';
 ?>
